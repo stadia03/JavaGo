18b988fb89695c699071950e773d5f9ff3988fcc
stadia03 <gaurab.working@gmail.com>
2024-08-26 21:47:50 +0530

    vercel updates

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 
 const mongoDB = require('./db');
 mongoDB();
+app.use(cors({
+  origin: ["https://deploy-mern-lwhq.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
